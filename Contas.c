4bfbc5ee8c3acb4fd8d5ead2f346478e16453816
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-03-04 00:14:26 -0300

    Update Contas.c

--- Contas.c
+++ Contas.c
@@ -1,1 +1,15 @@
-printf
+#include <studio.h>
+#include <stdlib.h>
+#include <math.h>
+
+   int main (void)
+     
+   {
+     float y;
+     int x;
+     
+     printf("O valor de x eh: %d \n \n ", 2 + 3 *(-5) );
+     
+     return 0;
+     
+   }
