45f15a971c6b9b8feb7c2ea2e2825b601ab2b6f9
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-03-18 01:27:07 -0300

    Update alturapeso.c

--- alturapeso.c
+++ alturapeso.c
@@ -3,3 +3,20 @@
 #include <locale.h>
 
 int main()
+  
+  printf("Este programa verifica o peso e a altura para servir o exercito. \n\n ");
+  float altura, peso;
+ 
+  printf("Digite o valor de sua altura em metros: \n") ;
+  scanf("%f",  &altura) ;
+
+  printf("Digite o valor de seu peso em kg: \n ");
+  scanf("%f", &peso);
+  setlocale(LC_ALL, "Portuguese");
+  if (altura >= 1.60 && peso >= 60.0) {
+    printf ("O candidato pode fazer inscrição. \n") ;
+    printf ("Parabens!\n");
+  }
+  else 
+      printf(" O candidato não passou na seleção") ;
+  return 0;
