d05b50bbb35e2fa25e36473eb437a4fad896e33f
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-03-19 21:01:17 -0300

    Update QuestaoMédia.c

--- QuestaoMédia.c
+++ QuestaoMédia.c
@@ -6,14 +6,15 @@
    int main ()
      
    {
-   print("Este programa calcular a média final na FICS");
-   float notap1. notap2, mult, media;
-   print("Digite a nota da P1 e tecler enter: \n");
+   print("Este programa calcula a média final na FICS");
+   float notap1, notap2, mult, media;
+   printf("Digite a nota da P1 e tecle enter: \n");
    scanf("%f",& notap1);
    printf("Digite a nota da P2 e tecle enter: \n");
    scanf("%f",& notap2);
-   print("Digite a nota da Multi e tecle Enter: \n");
+   printf("Digite a nota da Multi e tecle Enter: \n");
    media = (notap1+notap2+multi) /2;
-   print("A sua nota final eh:%.1f \n \n ",media);
+   printf("A sua nota final eh:%.1f \n \n ",media);
    get char();
    }
+   return 0;
