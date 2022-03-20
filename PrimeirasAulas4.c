12b5bbcf8ee67a2ed673e146e08afe479a911739
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-03-19 22:46:38 -0300

    Create PrimeirasAulas4.c

---
+++ PrimeirasAulas4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+ int main ()
+ {
+   int var;
+   float constante;
+   
+   printf("o valor da var eh : %d \n \n",7);
+   printf("o valor da constante eh : %f \n \n", 3.14);
+ }
+
+return 0;
