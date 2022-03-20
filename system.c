4af08d49938be4914bbc4e02f09a2a539b7ced8b
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-03-19 21:21:35 -0300

    Create system.c

---
+++ system.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#inlucde <stdlib.h>
+#include <math.h>
+
+{
+  float x;
+  
+  printf (" o valor de x eh %.6f \n\n\n"), 2.0/7 );
+  return 0;
+  
+  system("pause");
+}
+ return 0;
