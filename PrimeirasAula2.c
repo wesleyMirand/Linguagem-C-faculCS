8f95af88e4da60af94fa24c495cf1bdadff85f33
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-03-19 22:37:05 -0300

    Update PrimeirasAula2.c

--- PrimeirasAula2.c
+++ PrimeirasAula2.c
@@ -3,7 +3,7 @@
 
  int main (void)
   {
-   printf ("o preco de 1 caixa de leite eh R$ %.2f \n \n", (57.50 / 2.3));
+   printf ("o preco de 1 caixa de leite eh R$ %f .2f \n \n", (57.50 / 2.3));
    }
    return 0;
   
