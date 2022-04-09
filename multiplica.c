06f09e976ca37e9f9813a51684996e2cacd12336
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-04-08 23:52:55 -0300

    Update multiplica.c

--- multiplica.c
+++ multiplica.c
@@ -7,7 +7,7 @@ int main ()
  
  float x;
   
- printf("o valor de x eh %.1f \n \n ", (1-(1.0/2) / (1-1.0(1-(1.0/2)));
+ printf("o valor de x eh %.1f \n \n ", (1-(1.0/2) / (1-2.0(1-(1.0/2)));
                                         
                                         return 0;
 
